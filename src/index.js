8aa86ad3b7d0db8b2c1790c1768ec80cee717fbe
Diogenes Ferreira <95397773+diogenesferreirabr@users.noreply.github.com>
2023-02-23 19:18:42 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// importar o express para a constante
+const express = require('express') 
+// instancia o express dentro de uma constante 
+const app = express()
+
+//porta que usaaremos...
+const port = 3000
+
+// funçao do express que...
+app.listen(port, (req, res)) => {
+    console.log(`servidor rodando na porta ${port}`)
+}
+
+// ROTA GET Cliente
+app.get('/cliente',(req, res) => {
+    re.send("rota GET do cliente ativada")
+})
+
+//ROTA GET Livro
+app.get('/livro',(req, res) => {
+    res.send("rota GET do livro criada")
+})
+
+
